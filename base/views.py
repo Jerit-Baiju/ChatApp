72bf062f5fae33d07f270167f9955ab63c2504b9
Jerit Baiju <Jeritalumkal@gmail.com>
2023-03-17 07:39:09 +0000

    .

--- base/views.py
+++ base/views.py
@@ -3,6 +3,6 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-@login_required()
+@login_required('login_page')
 def index(request):
     return render(request, 'main.html')
--- main/settings.py
+++ main/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
-    "base.apps.BaseConfig"
+    "base.apps.BaseConfig",
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
