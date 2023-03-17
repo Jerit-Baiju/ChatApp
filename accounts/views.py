b56dc366340f1e12b40a05cc1c2a2d32da7e6bbc
Jerit Baiju <Jeritalumkal@gmail.com>
2023-03-17 08:00:14 +0000

    .

--- accounts/views.py
+++ accounts/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def login_page(request):
     return render(request, 'login.html')
+
+
+def register_page(request):
+    return render(request, 'register.html')
