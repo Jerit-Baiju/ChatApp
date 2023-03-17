3a38b2c1b9cbb0af4c721d60ba66db5a85f8a86a
Jerit Baiju <Jeritalumkal@gmail.com>
2023-03-17 10:33:22 +0000

    .

--- main/settings.py
+++ main/settings.py
@@ -95,15 +95,6 @@ DATABASES = {
 }
 
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '127.0.0.1:11211',
-        'TIMEOUT': 0,
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
